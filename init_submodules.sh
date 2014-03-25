ef55da72e4c8cd0acfb0ccb3ff5c20819d720ca2
Jonathan Leonard <johanatan@gmail.com>
2014-03-24 23:12:05 -0700

    Setup master branches on submodules from init script.

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 git pull && git submodule init && git submodule update && git submodule status
 
+for dir in `find ./containers/* -maxdepth 1 -type d`
+do
+  pushd $dir
+  git pull origin master
+  git checkout master
+  popd
+done
