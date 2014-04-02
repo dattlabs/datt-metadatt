132d14ab8945734ba7c3f02e72b7682a9b3cc1a8
John Albietz <inthecloud247@gmail.com>
2014-04-02 15:19:23 -0700

    updated instructions / comments.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+# By default docker builds will use the current directory name before the `-` separator.
+# By setting a DOCKERINDEX_LOCAL variable, the builds can be tagged with a specific name.
+
 set -e
 
 DIR=`pwd`
 CURRENT_DIR="${DIR##*/}"
-
-# The docker index server can be set to a local index, or by default it will
-# use the current directory name before the `-` separator.
-#    for example: datt-nginx will use the `datt` index account.
 
 INDEX_NAME=$(echo $CURRENT_DIR | cut -d- -f 1)
 DOCKERINDEX=${DOCKERINDEX_LOCAL:-"$INDEX_NAME/"}
