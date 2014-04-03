19d81149d0e7586e5f50459d3ce5d3dc2a06dd5c
Jonathan Leonard <johanatan@gmail.com>
2014-04-03 15:01:35 -0700

    Start container under test and wait for it to appear.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=`pwd`
 
+trap 'docker kill `cat host.id` &>/dev/null' EXIT INT TERM HUP
+make debug &>/dev/null &
+
+until [[ -s host.id ]]; do :; done
+
 pushd $SCRIPT_DIR > /dev/null
 bundle exec rspec -I $SCRIPT_DIR -I $DIR/spec --default-path $DIR
 popd > /dev/null
