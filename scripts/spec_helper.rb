d79cea73ece257f29a7cf521c6494122e0052c40
Jonathan Leonard <johanatan@gmail.com>
2014-04-03 22:13:50 -0700

    Run all tests in the containers /files/tests dir.

--- scripts/spec_helper.rb
+++ scripts/spec_helper.rb
@@ -6,3 +6,19 @@ require 'docker'
 
 Docker.url = "http://localhost:4243"
 
+containerDir = ENV['DIR']
+containerID = `cat #{containerDir}/host.id`
+testServerHostname, testServerPort = `docker port #{containerID} 13337`.split ':'
+
+tests = Dir.glob(containerDir + '/files/tests/*')
+
+tests.each { |testPath|
+  describe testPath do
+    it "should return exit code 0" do
+      test = Pathname.new(testPath).basename
+      print test
+      output = `./telnet.sh #{testServerHostname} #{testServerPort} #{test}`
+      print output
+    end
+  end
+}
