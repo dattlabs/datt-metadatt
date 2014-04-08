db4417aa882ecf96447de39a46c413403dfda88c
Jonathan Leonard <johanatan@gmail.com>
2014-04-07 19:54:50 -0700

    Add support for configuring docker run command via environment variables.

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,6 +12,14 @@ DOCKERINDEX=${DOCKERINDEX_LOCAL:-"$INDEX_NAME/"}
 
 DENV=`test $# -gt 0 && echo "--env=$*" || echo ''`
 
-trap 'rm $DIR/host.id' EXIT INT TERM HUP
+trap 'rm -f $DIR/host.id' EXIT INT TERM HUP
 
-docker run --cidfile=$DIR/host.id --publish=13337:13337 -i -t --rm -w "/files" $DENV --hostname $CURRENT_DIR $DOCKERINDEX$CURRENT_DIR bash -c "supervisord; /bin/bash"
+VOLUMES=`test -n "$VOLUMES" && echo "-v $VOLUMES" || echo ''`
+INPUT_FILE=${INPUT_FILE:-/dev/stdin}
+OUTPUT_FILE=${OUTPUT_FILE:-/dev/stdout}
+TTY=`test "$INPUT_FILE" = "/dev/stdin" && echo "-t" || echo ''`
+
+RUN_CMD="/files/start.sh"
+
+docker run --expose=13337 $TTY -a stdout -a stdin --cidfile=$DIR/host.id $VOLUMES -P -i --rm -w "/files" \
+  $DENV --hostname $CURRENT_DIR $DOCKERINDEX$CURRENT_DIR $RUN_CMD < "$INPUT_FILE" > "$OUTPUT_FILE"
