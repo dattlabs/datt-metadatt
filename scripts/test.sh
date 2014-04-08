aa3cf72ea6d42a4549ba7ff1f68be08cb9ba90c7
Jonathan Leonard <johanatan@gmail.com>
2014-04-07 19:54:20 -0700

    Wait for test_server to be accepting connections.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,10 +3,19 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export DIR=`pwd`
 
-trap 'docker kill `cat host.id` &>/dev/null' EXIT INT TERM HUP
-make debug &>/dev/null &
+trap 'docker kill `cat host.id 2>/dev/null` &>/dev/null' EXIT INT TERM HUP
+../../scripts/run.sh RUN_DEBUG=1 &
 
-until [[ -s host.id ]]; do :; done
+until [[ -s host.id ]]; do sleep 0.1; done
+
+containerID=`cat host.id`
+portOut=`docker port $containerID 13337`
+hostAndPort=(${portOut//:/ })
+while :; do
+  ../../scripts/telnet.sh ${hostAndPort[0]} ${hostAndPort[1]} bogus &>/dev/null
+  [[ $? -ne 0 ]] || break
+  sleep 0.25
+done
 
 pushd $SCRIPT_DIR > /dev/null
 bundle exec rspec -I $SCRIPT_DIR -I $DIR/spec --default-path $DIR
