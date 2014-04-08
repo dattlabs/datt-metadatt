3994a48a7c2085c6b955b27c89d6e1e2fc6a76c5
Jonathan Leonard <johanatan@gmail.com>
2014-04-07 19:55:04 -0700

    Finish up spec_helper.

--- scripts/spec_helper.rb
+++ scripts/spec_helper.rb
@@ -3,22 +3,23 @@ require 'serverspec'
 require 'pathname'
 require 'net/ssh'
 require 'docker'
+require 'json'
 
 Docker.url = "http://localhost:4243"
 
 containerDir = ENV['DIR']
 containerID = `cat #{containerDir}/host.id`
-testServerHostname, testServerPort = `docker port #{containerID} 13337`.split ':'
+testServerHostname, testServerPort = `docker port #{containerID} 13337`.strip!.split ':'
 
 tests = Dir.glob(containerDir + '/files/tests/*')
 
 tests.each { |testPath|
   describe testPath do
     it "should return exit code 0" do
-      test = Pathname.new(testPath).basename
-      print test
-      output = `./telnet.sh #{testServerHostname} #{testServerPort} #{test}`
-      print output
+      test = Pathname.new(testPath).basename.to_s
+      output = `./telnet.sh #{testServerHostname} #{testServerPort} #{test}`.to_s.strip!
+      res = JSON.parse output
+      res['code'].should equal(0)
     end
   end
 }
