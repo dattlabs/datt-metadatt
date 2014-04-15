0305554cd27d9a03c59bcae2f1f423b9faa890cd
Jonathan Leonard <johanatan@gmail.com>
2014-04-15 14:35:49 -0700

    Scripts to set port mapping tag on containers.

---
+++ scripts/set_ports.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import inspect
+import json
+import os
+
+def get_port_mappings(container):
+  script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+  container_id = os.popen('cat %s/../containers/%s/host.id' % (script_dir, container)).read().strip()
+  info = os.popen('docker inspect %s' % container_id).read().strip()
+
+  parsed = json.loads(info)[0]
+  return {k.split('/')[0]:v[0]['HostPort'] for k,v in parsed['NetworkSettings']['Ports'].items()}
+
+def rpc_port(container):
+  return get_port_mappings(container)['7373']
+
+if __name__ == '__main__':
+  from set_tag import set_tag, check_container_count
+
+  parser = OptionParser(usage="usage: %prog container")
+  (options, args) = parser.parse_args()
+
+  if len(args) != 1: parser.print_help(); exit(1)
+
+  container = args[0]
+  check_container_count(container)
+
+  set_tag(container, "ports", json.dumps(get_port_mappings(container)))
---
+++ scripts/set_tag.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+from set_ports import rpc_port
+import os
+
+def set_tag(container, key, value):
+  print "Setting tag '%s' on container '%s' to '%s'" % (key, container, value)
+  print os.popen('serf tags -set "%s"="%s" -rpc-addr=%s' % (key, value, '0.0.0.0:%s' % rpc_port(container)))
+
+def check_container_count(container):
+  output = os.popen('docker ps | grep %s' % container).read().strip()
+  count = len(output.splitlines()) 
+  if count != 1: raise RuntimeError('Error must be exactly one container of this type running but found %s' % count)
+
+if __name__ == "__main__":
+  parser = OptionParser(usage="usage: %prog container tag_key tag_value")
+  (options, args) = parser.parse_args()
+
+  if len(args) != 3: parser.print_help(); exit(1)
+
+  (container, tag, value) = (args[0], args[1], args[2])
+  check_container_count(container)
+
+  set_tag(container, tag, value)
