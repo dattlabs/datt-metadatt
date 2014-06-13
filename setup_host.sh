cbe32f0f26e46e970768a5f0e8cee0464f9eb6ae
Jonathan Leonard <johanatan@gmail.com>
2014-06-13 13:44:23 -0700

    Add serf install.

--- setup_host.sh
+++ setup_host.sh
@@ -9,3 +9,9 @@ sudo apt-get install make
 
 sudo gpasswd -a ${USER} docker
 sudo service docker restart
+
+wget https://dl.bintray.com/mitchellh/serf/0.6.1_linux_amd64.zip
+apt-get install unzip
+unzip 0.3.0_linux_amd64.zip
+sudo mv serf /usr/local/bin
+rm 0.6.1_linux_amd64.zip
